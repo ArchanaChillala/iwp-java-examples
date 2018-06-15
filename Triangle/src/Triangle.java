4b8745ca1fad1ee533461731c2a94df4296b5144
ArchanaChillala <archanac@thoughtworks.com>
2018-06-15 08:02:48 +0530

    Decomposed conditional for better readability

--- Triangle/src/Triangle.java
+++ Triangle/src/Triangle.java
@@ -1,18 +1,27 @@
 public class Triangle {
+
     private double side1;
     private double side2;
     private double side3;
 
     public Triangle(double side1, double side2, double side3) throws TriangleException {
-        if(side1 == 0 || side2 == 0 || side3 == 0) {
+        if(allSidesAreZero(side1, side2, side3)) {
             throw new TriangleException();
         }
-        if(side1 + side2 < side3 || side2 + side3 < side1) {
+        if(triangleEqualityNotSatisfied(side1, side2, side3)) {
             throw new TriangleException();
         }
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
+    }
+
+    private boolean allSidesAreZero(double side1, double side2, double side3) {
+        return side1 == 0 || side2 == 0 || side3 == 0;
+    }
+
+    private boolean triangleEqualityNotSatisfied(double side1, double side2, double side3) {
+        return side1 + side2 < side3 || side2 + side3 < side1;
     }
 
 
@@ -25,6 +34,6 @@ public class Triangle {
     }
 
     boolean isScalene() {
-        return side1 != side2 && side2 != side3;
+        return !isEquilateral() && !isIsosceles();
     }
 }
